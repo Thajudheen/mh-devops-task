2d973692f9c33ce8555e0a9df422074888e01d6e
thaj <hellothaj@gmail.com>
2021-08-05 15:23:11 +0200

    Push code for cleanup

--- task1/cleanup.sh
+++ task1/cleanup.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+find_hidden () {
+find /opt -name ".prune-enable"
+return 0
+}
+find_hidden
+echo The previous function has a return value of $?
 
-# Your script here
+ delete_hidden () {
+ find /opt -name ".crash.dump" -type f -exec rm -f {} \; -print
+ return 0
+ }
+ delete_hidden
+ echo The previous function has a return value of $?
+
+ find_log () {
+ filepath=/opt
+ var1="$(basename $(find "$filepath" -size +1M -type f \( -iname "*.log"  \) | awk '{print $NF}'))"
+ echo "$filepath/$var1"
+ tail -n 20000 "$filepath/$var1" >> "$filepath"/newfile
+ return 0
+ }
+ find_log
